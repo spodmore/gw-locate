21c8be25a0dbd94160bf5fd3ea9db82a8c110b64
spodmore <spod.corp@gmail.com>
2017-09-05 15:03:21 +0300

    Initial commit: working version 1

---
+++ background.js
@@ -0,0 +1,33 @@
+const GM_REGEXP_STRING =
+    "google.(com.(?!ru)[a-z]{2}|co.[a-z]{2}|com|(?!ru)[a-z]{2})\/maps\/.*\\@(\\d+\\.\\d+),(\\d+\\.\\d+),(\\d+z)?.*";
+const GM_REGEXP = new RegExp(GM_REGEXP_STRING);
+const WIKIMAPIA_BASE_URL_HASH = "http://wikimapia.org/#";
+
+function checkForValidUrl(tabId, changeInfo, tab)
+{
+    if(typeof tab != "undefined" && typeof tab != "null" )
+    {
+        if (GM_REGEXP.test(tab.url))
+        {
+            chrome.pageAction.show(tabId);
+        }
+    }
+};
+
+function locateToWikimapia(tab) {
+    var urlMatchParts = tab.url.match(GM_REGEXP);
+    [lat, lon, zoom] = [urlMatchParts[2], urlMatchParts[3], urlMatchParts[4]];
+    var targetURL = WIKIMAPIA_BASE_URL_HASH + "lat=" + lat + "&lon=" + lon;
+    if (zoom) {
+        targetURL += "&z=" + zoom;
+    }
+    var creating = browser.tabs.create({
+        url: targetURL,
+        active: true
+    });
+};
+
+// Listen for any changes to the URL of any tab.
+browser.tabs.onUpdated.addListener(checkForValidUrl);
+
+browser.pageAction.onClicked.addListener(locateToWikimapia);
